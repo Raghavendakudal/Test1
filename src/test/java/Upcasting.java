13fa6107acaffdd87fe365a3ae6275504743fa18
D1RQ <D1RQ@IN01LAP80205.area1.eurofins.local>
2019-11-26 16:06:20 +0530

    Add new test case about casting

---
+++ src/test/java/Upcasting.java
@@ -0,0 +1,27 @@
+
+public class Upcasting {
+
+	public static void main(String[] args) {
+
+		/*
+		 * int i = 10; double d = i; long l = i; System.out.println("value of d is:" +
+		 * d); System.out.println("value of l is :" + l);
+		 * 
+		 * long L = 2000; int n = (int) L; System.out.println("value of n is:" + n);
+		 */
+
+		/*
+		 * String s = "10"; int a = Integer.parseInt(s);
+		 * System.out.println("value of a is:" + a);
+		 * 
+		 * long b = Long.parseLong(s); System.out.println("value of b is :" + b);
+		 * 
+		 * double c = Double.parseDouble(s); System.out.println("value of c is :" + c);
+		 */
+
+		int z = 25;
+		String ss = String.valueOf(z);
+		System.out.println(ss);
+	}
+
+}
